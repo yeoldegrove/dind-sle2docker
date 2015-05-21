2f24a56d0f93edaa9da58b23791f1a0996172ac9
Eike Waldt <git@yeoldegrove.de>
2015-05-21 13:56:09 +0200

    modified:   build-sle2docker.sh 	modified:   patch_wrapdocker.patch

--- build-sle2docker.sh
+++ build-sle2docker.sh
@@ -5,10 +5,10 @@ patch -p2 wrapdocker <patch_wrapdocker.patch
 patch -p2 Dockerfile <patch_Dockerfile.patch
 docker build -t sle2docker .
 if [ $? -eq 0 ]; then
-    echo "$(tput setab 0)$(tput setaf 1)opensuse image for sle2docker created$(tput sgr 0)" && \
-    echo "$(tput setab 0)$(tput setaf 1)to build a SLE container, use it like:$(tput sgr 0)" && \
-    echo "$(tput setab 0)$(tput setaf 1)# docker run -t -i --privileged sle2docker sle2docker build -s <repo IP/url> (--disable-https) SLE11SP2|SLE11SP3|SLE12$(tput sgr 0)"
+    echo "$(tput setaf 1)opensuse image for sle2docker created$(tput sgr 0)" && \
+    echo "$(tput setaf 1)to build a SLE container, use it like:$(tput sgr 0)" && \
+    echo "$(tput setaf 1)# docker run -t -i --privileged sle2docker sle2docker build -s <repo IP/url> (--disable-https) SLE11SP2|SLE11SP3|SLE12$(tput sgr 0)"
 else
-    echo "$(tput setab 0)$(tput setaf 1)something went wrong"
+    echo "$(tput setaf 1)something went wrong"
 fi
 rm -rf wrapdocker dind Dockerfile
