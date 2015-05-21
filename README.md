48b01fa78f2a3ed3f52b22f287b39ad082dab4e4
Eike Waldt <git@yeoldegrove.de>
2015-05-21 12:28:15 +0200

    inital commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # dind-sle2docker
+This is a set of patches to build a SLE docker image.
+It is based on https://github.com/jpetazzo/dind
---
+++ build-sle2docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+test -d dind || git clone https://github.com/yeoldegrove/dind.git
+cp dind/wrapdocker dind/opensuse/Dockerfile .
+patch -p2 wrapdocker <patch_wrapdocker.patch
+patch -p2 Dockerfile <patch_Dockerfile.patch
+docker build -t sle2docker .
+if [ $? -eq 0 ]; then
+    echo "$(tput setab 0)$(tput setaf 1)opensuse image for sle2docker created$(tput sgr 0)" && \
+    echo "$(tput setab 0)$(tput setaf 1)to build a SLE container, use it like:$(tput sgr 0)" && \
+    echo "$(tput setab 0)$(tput setaf 1)# docker run -t -i --privileged sle2docker sle2docker build -s <repo IP/url> (--disable-https) SLE11SP2|SLE11SP3|SLE12$(tput sgr 0)"
+else
+    echo "$(tput setab 0)$(tput setaf 1)something went wrong"
+fi
+#rm -rf wrapdocker dind Dockerfile
